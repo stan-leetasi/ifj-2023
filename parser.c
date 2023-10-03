a9a293dbf817a00c73d60b14255a36e4d5f57410
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-10-03 12:03:00 +0200

    parser súbory

---
+++ parser.c
@@ -0,0 +1,10 @@
+/** Projekt IFJ2023
+ * @file parser.c
+ * @brief Syntaktický a sémantický anayzátor
+ * @author 
+ * @date 
+ */
+
+#include "parser.h"
+
+/* Koniec súboru parser.c */
---
+++ parser.h
@@ -0,0 +1,12 @@
+/** Projekt IFJ2023
+ * @file parser.h
+ * @brief Syntaktický a sémantický anayzátor
+ * @author 
+ * @date 
+ */
+
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+#endif // ifndef _PARSER_H_
+/* Koniec súboru parser.h */
