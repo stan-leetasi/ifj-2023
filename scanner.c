b0887f9c8719a38432aec7f9acc76eab25e76710
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-10-03 11:19:39 +0200

    scanner.c

---
+++ scanner.c
@@ -0,0 +1,24 @@
+/** Projekt IFJ2023
+ * @file scanner.c
+ * @brief Lexikálny analyzátor - skener tokenov
+ * @author 
+ * @date 
+ */
+
+#include "scanner.h"
+#include "stdlib.h"
+
+token_T *storage = NULL; ///< úschovňa pre jeden token
+
+
+token_T *getToken()
+{
+    return NULL;
+}
+
+void storeToken(token_T *tkn)
+{
+
+}
+
+/* Koniec súboru scanner.c */
