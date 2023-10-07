be3d2bbeb2f86595b628d6e109d2b059258226cc
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-10-07 21:26:21 +0200

    Upravené testovacie skripty

--- test.sh
+++ test.sh
@@ -7,12 +7,17 @@
 
 make || exit 1
 if [ $# -gt 0 ]; then
+    if [ ! -d "tests/$1" ]; then 
+        echo "Incorrect argument"
+        exit 1
+    fi
     for f in *.o
     do
         cp "${f}" "tests/$1"
     done
     cd "tests/$1" || exit 1
-    bash "test.sh"
+    rm -f "test.result"
+    bash "test.sh" 2> "test.result"
 else
     echo "enter test name"
 fi
--- tests/scanner/test.sh
+++ tests/scanner/test.sh
@@ -2,16 +2,25 @@
 
 make test.out  || exit 1
 echo "Executing scanner unit tests"
-for f in ./*.sample
+total=0
+passed=0
+for f in *.sample
 do
+    total=$((total + 1))
     test_sample=${f%.*}
     ./test.out <"${test_sample}.sample" >"${test_sample}.result"
     DIFF="$(diff "${test_sample}.exp" "${test_sample}.result")"
     if [ "${DIFF}" == "" ]; then 
         echo -n "[PASS]"
+        passed=$((passed + 1))
     else
         echo -n "[FAIL]"
     fi
     echo " test #${test_sample}"
 done
+if [ "${total}" -eq "${passed}" ]; then
+    echo "Everything OK"
+else
+    echo "Some tests failed ${passed}/${total}"
+fi
 make clean
