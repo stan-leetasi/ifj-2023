6d5d1b636dbd00c1e0967d2be0b8b39587c27b48
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-10-22 14:16:30 +0200

    pridané funkcie do symtable.h

--- symtable.h
+++ symtable.h
@@ -8,7 +8,7 @@
 #ifndef _SYMTABLE_H_
 #define _SYMTABLE_H_
 
-#include "strR.h"
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define SYM_TYPE_FUNC       'F'
@@ -20,6 +20,8 @@
 #define SYM_TYPE_STRING_NIL 'S'
 #define SYM_TYPE_VOID       'V'
 #define SYM_TYPE_UNKNOWN    'U'
+
+#define SYMTABLE_MAX_SIZE 997 // musí byť prvočíslo
 
 /**
  * @brief Signatúra funkcie
@@ -36,11 +38,11 @@ typedef struct func_signature {
  * @brief Dátový element / prvok tabuľky symbolov, obsahuje informácie o symbole/identifikátore premennej alebo funkcie
  */
 typedef struct TSData {
-    str_T id;       ///< názov identifikátoru, zároveň kľúč v tabuľke
+    char *id;       ///< názov identifikátoru, zároveň kľúč v tabuľke
     char type;      ///< typ premennej/funkcia, používa hodnoty SYM_TYPE_XXX
     bool let;       ///< true znamená premenná let inak var 
     bool init;      ///< true znamená, že je premenná inicializovaná alebo funkcia definovaná
-    func_sig_T sig;///< signatúra funkcie, v prípade premennej sig=NULL
+    func_sig_T sig; ///< signatúra funkcie, v prípade premennej sig=NULL
 } TSData_T;
 
 /**
@@ -55,11 +57,75 @@ typedef struct TSBlock {
 
 /**
  * @brief Tabuľka symbolov
+ * @details Ak tabuľka obsahuje len globálny rámec, potom local ukazuje aj na globálny.
  */
 typedef struct SymbolsTable {
     TSBlock_T *global;      ///< ukazateľ na globálny blok (prvý blok)
     TSBlock_T *local;       ///< ukazateľ na lokálny blok  (posledný blok)
-} *SymTab_T;
+} SymTab_T;
+
+
+/**
+ * @brief Inicializuje tabuľku symbolov a vloží do nej jeden globálny rámec
+ * @return true v prípade úspechu, inak false
+*/
+bool SymTabInit(SymTab_T *st);
+
+/**
+ * @brief Vytvorí a pridá do tabuľky symbolov nový dielčí lokálny rámec/blok
+ * @return true v prípade úspechu, inak false
+*/
+bool SymTabAddLocalBlock(SymTab_T *st);
+
+/**
+ * @brief Uvoľní všetok obsah alokovaný v poslednom lokálnom bloku tabuľky a odstráni tento blok z tabuľky.
+*/
+void SymTabRemoveLocalBlock(SymTab_T *st);
+
+/**
+ * @brief Dealokuje všetok obsah tabuľky symbolov.
+*/
+void SymTabDestroy(SymTab_T *st);
+
+/**
+ * @brief Vyhľadá tabuľke symbolov prvú položku s daným kľúčom/symbolom. Vyhľadáva od posledného lokálneho až po globálny.
+ * @param st tabuľka symbolov
+ * @param key hľadaný klúč/symbol
+ * @return položka s daným kľúčom alebo NULL ak sa v tabuľke nenachádza
+*/
+TSData_T *SymTabLookup(SymTab_T *st, char *key);
+
+/**
+ * @brief Vyhľadá len v globálnom bloku tabuľky symbolov položku s daným kľúčom/symbolom.
+ * @param st tabuľka symbolov
+ * @param key hľadaný klúč/symbol
+ * @return položka s daným kľúčom alebo NULL ak sa v tabuľke nenachádza
+*/
+TSData_T *SymTabLookupGlobal(SymTab_T *st, char *key);
+
+/**
+ * @brief Vyhľadá len v lokálnom bloku tabuľky symbolov položku s daným kľúčom/symbolom.
+ * @param st tabuľka symbolov
+ * @param key hľadaný klúč/symbol
+ * @return položka s daným kľúčom alebo NULL ak sa v tabuľke nenachádza
+*/
+TSData_T *SymTabLookupLocal(SymTab_T *st, char *key);
+
+/**
+ * @brief Vloží do globálneho bloku tabuľky symbolov nový prvok.
+ * @param st tabuľka symbolov
+ * @param elem vkladaný prvok
+ * @return true v prípade úspechu, inak false
+*/
+bool SymTabInsertGlobal(SymTab_T *st, TSData_T *elem);
+
+/**
+ * @brief Vloží do posledného lokálneho bloku tabuľky symbolov nový prvok.
+ * @param st tabuľka symbolov
+ * @param elem vkladaný prvok
+ * @return true v prípade úspechu, inak false
+*/
+bool SymTabInsertLocal(SymTab_T *st, TSData_T *elem);
 
 #endif // ifndef _SYMTABLE_H_
 /* Koniec súboru symtable.h */
