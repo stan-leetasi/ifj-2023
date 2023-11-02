45907464608721b84087688542725dde1ef8f03c
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-11-02 16:22:05 +0100

    pridané súbory logErr

---
+++ logErr.c
@@ -0,0 +1,8 @@
+/** Projekt IFJ2023
+ * @file logErr.c
+ * @brief Funkcie pre hlásenie chýb prekladu kódu či samotného prekladača
+ * @author Michal Krulich
+ * @date 02.11.2023
+ */
+
+/* Koniec súboru logErr.c */
---
+++ logErr.h
@@ -0,0 +1,13 @@
+/** Projekt IFJ2023
+ * @file logErr.h
+ * @brief Funkcie pre hlásenie chýb prekladu kódu či samotného prekladača
+ * @author Michal Krulich
+ * @date 02.11.2023
+ */
+
+#ifndef _LOGERR_H_
+#define _LOGERR_H_ 1
+
+#endif
+
+/* Koniec súboru logErr.h */
