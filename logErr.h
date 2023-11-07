bd4f213670e61d6dc7588009fda69bb9d7fc4898
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-11-07 16:10:01 +0100

    Pridané funkcie pre hlásenie chýb

--- logErr.h
+++ logErr.h
@@ -8,6 +8,16 @@
 #ifndef _LOGERR_H_
 #define _LOGERR_H_ 1
 
+/**
+ * @brief Vypíše do stderr chybu prekladača s priloženou správou
+*/
+void logErrCompiler(const char *msg);
+
+/**
+ * @brief Vypíše do stderr chybu prekladu kódu, funguje podobne ako printf
+*/
+void logErrCodeAnalysis(const int err_code, const size_t ln, const size_t c, const char *format, ...);
+
 #endif
 
 /* Koniec súboru logErr.h */
