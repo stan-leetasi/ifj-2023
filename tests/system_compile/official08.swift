8936df6f3f4e5a4ce36b26ba723ebb663b546e82
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-11-22 12:04:09 +0100

    skener testy, uprava 08.official.swift

--- tests/system_compile/official08.swift
+++ tests/system_compile/official08.swift
@@ -1,9 +1,9 @@
 // Ukazka prace s retezci a vestavenymi funkcemi 
 
-write("Ahoj\n\"Sve'te \\\u{0022}", "\n")  // 042 oktalove = 34 decimalne = 22 hexadecimalne = znak uvozovek
+write("Ahoj\n\"Sve'te \\\u{22}", "\n")  // 042 oktalove = 34 decimalne = 22 hexadecimalne = znak uvozovek
 
 var s1 : String? = 
- """
+  """
    Toto 
    je 
 
@@ -11,7 +11,7 @@ var s1 : String? =
    text
  """  // ukoncujici uvozovky ovlivnuji implicitni odsazeni vnitrnich radku retezce
 var s2:String = (s1!) + ", ktery jeste trochu obohatime"
-write(s1, "\u{000020}", s2, "\u{0A}")
+write(s1, "\u{20}", s2, "\u{0A}")
 
 var s1String = s1!
 var s1len = length(s1String)
@@ -21,7 +21,8 @@ let s1_16ty_znak : Int = s1_20ty_znak - s1len
 let s11 = substring(of: s2, startingAt: s1_16ty_znak, endingBefore: s1_20ty_znak)
 write(s1len, " znaky od ", 16, ". znaku (index ", s1_16ty_znak, ") v \"", s2, "\":", s11, "\n")
 
-s1String = substring(of:s2,startingAt:15,endingBefore:19) ?? 
+var medzivyseldok = substring(of:s2,startingAt:15,endingBefore:19)
+s1String = medzivyseldok ?? 
 """
 viceradkovy
 "retezec"
