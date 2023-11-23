cbc5cab7baa0711a7b089bc15183ec3d9ab97bfe
boris <borys.hatala@gmail.com>
2023-11-23 17:01:54 +0100

    testy update

--- tests/system_compile/b_06.swift
+++ tests/system_compile/b_06.swift
@@ -1,7 +1,7 @@
 var stringExample : String = "Swift ifj 2023!"
 
-// let stringLength = length(stringExample)
-// write("String Length: ", stringLength, "\n")
+let stringLength = length(stringExample)
+write("String Length: ", stringLength, "\n")
 
 var string = substring(of:stringExample,startingAt:0,endingBefore:5)
 write(string)
---
+++ tests/system_compile/b_10.swift
@@ -0,0 +1,26 @@
+var string : String? = "Hello, world!"
+
+var substring0 = substring(of:string, startingAt: -1, endingBefore: 5)
+// var test0 = substring0 ?? "nil"
+// write(test0, "\n")
+write(substring0, "\n")
+
+// var substring = substring(of:string, startingAt: -1, endingBefore: 5)
+// var test = substring ?? "nil"
+// write(test, "\n")
+
+// var substring2 = substring(of:string, startingAt: 0, endingBefore: 50)
+// var test2 = substring2 ?? "nil"
+// write(test2, "\n")
+
+// var substring3 = substring(of:string, startingAt: 0, endingBefore: -1)
+// var test3 = substring3 ?? "nil"
+// write(test3, "\n")
+
+// var substring4 = substring(of:string, startingAt: 6, endingBefore: 4)
+// var test4 = substring4 ?? "nil"
+// write(test4, "\n")
+
+// var substring5 = substring(of:string, startingAt: 13, endingBefore: 5)
+// var test5 = substring5 ?? "nil"
+// write(test5, "\n")
